6557188262803a7d05544744838eb488ef658a3a
JoseSholly <josepholawuyi30@gmail.com>
2023-09-14 11:44:28 +0100

    change-pasword-url

--- social_website/bookmarks/account/urls.py
+++ social_website/bookmarks/account/urls.py
@@ -5,9 +5,17 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns= [
+    # Previous log-in url
     # path('login/', views.user_login, name= 'login'),
 
+
+
+    #  Log-in and Log-out urls
     path('login/', auth_views.LoginView.as_view(), name= 'login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+
+    # change passowrd urls
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
 ]
