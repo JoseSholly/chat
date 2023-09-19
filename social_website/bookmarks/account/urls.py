c63d37d4943eca9644abd02638ee4e3e215b4c18
JoseSholly <josepholawuyi30@gmail.com>
2023-09-19 20:33:00 +0100

    edit-view-url-pattern

--- social_website/bookmarks/account/urls.py
+++ social_website/bookmarks/account/urls.py
@@ -26,5 +26,6 @@ urlpatterns= [
     # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 
     path('', include('django.contrib.auth.urls')), 
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit')
 ]
