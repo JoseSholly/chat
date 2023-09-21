a5181788879cc066a122d01ce0b2659230a51090
JoseSholly <josepholawuyi30@gmail.com>
2023-09-22 00:03:10 +0100

    custom-authentification-backend

--- social_website/bookmarks/account/views.py
+++ social_website/bookmarks/account/views.py
@@ -66,7 +66,7 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Profile updated'\
+            messages.success(request, 'Profile updated '\
                              'successfully')
     else:
         user_form= UserEditForm(instance=request.user)
