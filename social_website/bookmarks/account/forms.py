9bef5a36e6029628fe135cd104a18745001f13e1
JoseSholly <josepholawuyi30@gmail.com>
2023-09-22 00:16:34 +0100

    init

--- social_website/bookmarks/account/forms.py
+++ social_website/bookmarks/account/forms.py
@@ -20,6 +20,11 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match')
         return cd['password2']
+    def clean_email(self):
+        data= self.cleaned_data['email']
+        if User.objects.filter(email= data).exists():
+            raise forms.ValidationError('Emai already in use.')
+        return data
 
 class UserEditForm(forms.ModelForm):
     class Meta:
@@ -30,6 +35,8 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model= Profile
         fields= ['date_of_birth', 'photo']
+
+
 
 
     
