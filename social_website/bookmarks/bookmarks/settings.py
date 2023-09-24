2811229ad5b01db8a5a89454a8241645e1cf945f
JoseSholly <josepholawuyi30@gmail.com>
2023-09-24 19:33:18 +0100

    migration-init

--- social_website/bookmarks/bookmarks/settings.py
+++ social_website/bookmarks/bookmarks/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_django',
     'django_extensions',
-    'images.apps.ImageConfig'
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
