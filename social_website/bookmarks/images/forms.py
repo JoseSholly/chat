7862212689527dc447b0b9bad44a823088dc07b2
JoseSholly <josepholawuyi30@gmail.com>
2023-09-24 21:33:08 +0100

    overrinding-save-method

--- social_website/bookmarks/images/forms.py
+++ social_website/bookmarks/images/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Image
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 
-class ImageCreateForm(forms.Modelform):
+class ImageCreateForm(forms.ModelForm):
     class Meta:
         model= Image
         fields= ['title', 'url', 'description']
@@ -18,3 +21,21 @@ class ImageCreateForm(forms.Modelform):
             raise forms.ValidationError('The given URL does not '\
                                         'match valid image extensions.')
         return url
+    
+    # Overriding save method
+    def save(self, force_insert=False,
+                    force_update= False,
+                    commit=True):
+        image= super().save(commit=False)
+        image_url= self.cleaned_data['url']
+        name= slugify(image.title)
+        extension= image_url.rsplit('.', 1)[1].lower()
+        image_name= f'{name}.{extension}'
+
+        # Download image from the given url
+        response= requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save= False)
+
+        if commit:
+            image.save()
+        return image
--- social_website/bookmarks/images/views.py
+++ social_website/bookmarks/images/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
 # Create your views here.
+@login_required
+def image_created(request):
+    if request.method=='POST':
+        # form is sent
+        form = ImageCreateForm(data= request.POST)
+        if form.is_valid():
+            # form data is valid
+            cd= form.cleaned_data
+            new_image= form.save(commit=False)
+            # Assign the current user to the item
+            new_image.user= request.user
+            new_image.save()
+            messages.success(request, 'Image added successfu;ly')
+            # Redirect to new created item detail view
+            return redirect(new_image.get_absolute_url())
+    else:
+        # build form with data provided by the bookmarks via GET
+        form= ImageCreateForm(data= request.GET)
+    return render(request, 'images/image/create.html',
+                    {'section': 'images',
+                    'form': form})
