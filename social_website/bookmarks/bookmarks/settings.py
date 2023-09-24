f316351c9878e652b28761936a2f0c9a0f2fd339
JoseSholly <josepholawuyi30@gmail.com>
2023-09-24 13:37:43 +0100

    adding-social-auth-pipeline-profile-creation

--- social_website/bookmarks/bookmarks/settings.py
+++ social_website/bookmarks/bookmarks/settings.py
@@ -164,3 +164,16 @@ SOCIAL_AUTH_TWITTER_SECRET= os.getenv('TWITTER_KEY_SECRET')
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY= os.getenv('GOOGLE_CLIENT_ID')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET= os.getenv('GOOGLE_CLIENT_SECRET')
 # SOCIAL_AUTH_RAISE_EXCEPTIONS = False
+
+
+SOCIAL_AUTH_PIPELINE = [
+ 'social_core.pipeline.social_auth.social_details',
+ 'social_core.pipeline.social_auth.social_uid',
+ 'social_core.pipeline.social_auth.auth_allowed',
+ 'social_core.pipeline.social_auth.social_user',
+ 'social_core.pipeline.user.get_username',
+ 'social_core.pipeline.user.create_user',
+ 'social_core.pipeline.social_auth.associate_user',
+ 'social_core.pipeline.social_auth.load_extra_data',
+ 'social_core.pipeline.user.user_details',
+]
