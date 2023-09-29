7e6f83b867075c2490a12f61af0c38bfaf417c6e
JoseSholly <josepholawuyi30@gmail.com>
2023-09-29 08:53:10 +0100

    fixed-bug

--- social_website/bookmarks/account/models.py
+++ social_website/bookmarks/account/models.py
@@ -9,37 +9,30 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
-
-
-
-
-
-
 class Profile(models.Model):
-    user= models.OneToOneField(settings.AUTH_USER_MODEL,
-                               on_delete=models.CASCADE)
-    date_of_birth= models.DateField(blank=True, null=True)
-    photo= models.ImageField(upload_to='user/%Y/%m/%d/', blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return f"Profile of {self.username,}"
 
 
 class Contact(models.Model):
-    user_from= models.ForeignKey('auth.User',
-                                related_name= 'rel_from_set',
-                                on_delete= models.CASCADE)
-    user_to= models.ForeignKey('auth.User',
-                               related_name='rel_to_set',
-                               on_delete=models.CASCADE)
-    created= models.DateTimeField(auto_now_add=True)
-
+    user_from = models.ForeignKey('auth.User',
+                                  related_name='rel_from_set',
+                                  on_delete=models.CASCADE)
+    user_to = models.ForeignKey('auth.User',
+                                related_name='rel_to_set',
+                                on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        indexes= [
+        indexes = [
             models.Index(fields=['-created']),
         ]
-        ordering= ['-created']
+        ordering = ['-created']
 
         def __str__(self) -> str:
             return f"{self.user_from} follows {self.user_to}"
@@ -49,6 +42,6 @@ user_model = get_user_model()
 
 user_model.add_to_class('following',
                         models.ManyToManyField('self',
-                           through=Contact,
-                           related_name='followers',
-                           symmetrical=False))
+                                               through=Contact,
+                                               related_name='followers',
+                                               symmetrical=False))
