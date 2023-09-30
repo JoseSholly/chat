28fa5b558d3645f65b2a04e1e6ab4588a3f49048
JoseSholly <josepholawuyi30@gmail.com>
2023-09-30 19:06:26 +0100

    adding-actions-model

--- social_website/bookmarks/actions/models.py
+++ social_website/bookmarks/actions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Actions(models.Model):
+    user= models.ForeignKey('auth.user',
+                            related_name='actions',
+                            on_delete=models.CASCADE,)
+    verb= models.CharField(max_length=225)
+    created= models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes= [
+            models.Index(fields=['-created'])
+        ]
+
+        ordering= ['-created']
