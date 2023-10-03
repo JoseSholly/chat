916e9dee646b6739b4fd9dad2f80ee864f3a9833
JoseSholly <josepholawuyi30@gmail.com>
2023-10-04 00:59:13 +0100

    installation-of-django-debug

--- social_website/bookmarks/bookmarks/settings.py
+++ social_website/bookmarks/bookmarks/settings.py
@@ -46,10 +46,12 @@ INSTALLED_APPS = [
     'images.apps.ImagesConfig',
     'easy_thumbnails',
     'actions.apps.ActionsConfig',
+    'debug_toolbar',
 
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -195,3 +197,8 @@ if DEBUG:
     import mimetypes
     mimetypes.add_type('application/javascript', '.js', True)
     mimetypes.add_type('text/css', '.css', True)
+
+
+INTERNAL_IPS= [
+    '127.0.0.1',
+               ]
